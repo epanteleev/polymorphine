6b1060beb334eee3b982902a6ee06f526c247269
epanteleev <msl.epanteleev@gmail.com>
2025-08-03 20:58:10 +0300

    More work

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -1,5 +1,5 @@
 
-# CMake module to register sanitizers for debugging
+# Register new build types for sanitizers and coverage
 function(register_build_type name flags)
     string(TOUPPER ${name} PREFIX)
     message(STATUS "Registering sanitizer: ${PREFIX} with flags: ${flags}")
@@ -13,6 +13,21 @@ function(register_build_type name flags)
     set(CMAKE_SHARED_LINKER_FLAGS_${PREFIX} "${CMAKE_SHARED_LINKER_FLAGS_DEBUG} ${flags}" PARENT_SCOPE)
 endfunction()
 
+# Try to set up coverage options for a target if '-DCMAKE_BUILD_TYPE=Coverage'
+function(try_setup_coverage_options target)
+    if (NOT CMAKE_BUILD_TYPE STREQUAL "Coverage")
+        message(STATUS "Skipping coverage setup for target ${target} as CMAKE_BUILD_TYPE is not 'Coverage'")
+        return()
+    endif ()
+
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+        target_compile_options(${target} PRIVATE -fprofile-arcs -ftest-coverage)
+        target_link_options(${target} PRIVATE -fprofile-arcs -ftest-coverage)
+    else()
+        message(WARNING "Coverage options are not set for compiler: ${CMAKE_CXX_COMPILER_ID}")
+    endif()
+endfunction()
+
 register_build_type(Asan "-fsanitize=address")
 register_build_type(Usan "-fsanitize=undefined")
-register_build_type(Coverage "-fprofile-arcs -ftest-coverage")
+register_build_type(Coverage "") # No specific flags for coverage, handled in try_setup_coverage_options
